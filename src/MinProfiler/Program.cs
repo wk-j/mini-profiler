05b4b96eb5c657f7d17e99a08d48c196f10438fa
wk-j <somnuk.wk@gmail.com>
2018-08-22 02:47:15 +0700

    Initial commit

---
+++ src/MinProfiler/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using StackExchange.Profiling;
+
+namespace MinProfiler {
+    class Program {
+        static void Main(string[] args) {
+            var profiler = MiniProfiler.StartNew("My Profile");
+            using (profiler.Step("Hello, world!")) {
+                var s = String.Format("{0} {1} {2}", 1, 2, 3);
+                var d = DateTime.Now.ToString("yyyy MM dd");
+            }
+
+            Console.WriteLine(MiniProfiler.Current.RenderPlainText());
+        }
+    }
+}
